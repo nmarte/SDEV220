04e0c63435dc7153bb8c02bc1793d701f9caab66
Nelson Marte <nmarte@ivytech.edu>
2025-04-07 23:08:03 -0400

    M03 assn commit

---
+++ M03/Assn/M03_Tut_Func_vs_OOP_Prog.py
@@ -0,0 +1,22 @@
+'''
+M03 Programming Assignment - Lists and Functions
+Author: Nelson Marte
+completed 04-07-2025
+Assignment:
+Complete the following steps:
+OOP Programming:
+Navigate to the following URL: Geeks for Geeks Classes and ObjectsLinks to an external site.
+You will need to sign in to GeekforGeeks after clicking on this link. This is a free service that is also an excellent resource for learning about all things coding.
+Read through the information related to classes and objects and examine the examples. 
+Complete the programming exercise - Sort an Array of 0s, 1s, and 2sLinks to an external site.. Make sure that you choose Python 3 as the language for the exercise (in the top left of the code window)
+If you need help with the code, you may click the Editorial tab in the left window (the instructions window) to get a walk-through of the code. 
+Functional Programming
+Navigate to the following URL: Geeks for Geeks Lambda ExpressionsLinks to an external site.
+You will need to sign in to GeekforGeeks after clicking on this link. This is a free service that is also a great resource for learning about all things coding.
+Read through the information related to Lambda Expressions and examine the examples. 
+Complete the programming exercise - Binary SearchLinks to an external site.. Make sure that you choose Python 3 as the language for the exercise (in the top left of the code window)
+If you need help with the code, you may click the Editorial tab in the left window (the instructions window) to get a walk-through of the code. 
+There will be two exercises that you will need to turn in:
+Sort an Array of 0s, 1s, and 2s
+Binary Search
+'''
